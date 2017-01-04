1931818b6d470eff5b1951486bc7a0152ea929cb
lcom81_two <lcom81_two>
2017-01-04 12:37:20 +0530

    Second php file added to the project

---
+++ second.php
@@ -0,0 +1,17 @@
+<html>
+<head></head>
+<body>
+<form method="post" action="">
+<input type="text" name="fname" placeholder="First Name"/>
+<input type="text" name="lname" placeholder="Last Name"/>
+<input type="submit" name="sub"/>
+<hr>
+<form method="post" action="">
+<input type="text" name="fname" placeholder="First Name"/>
+<input type="text" name="lname" placeholder="Last Name"/>
+<input type="submit" name="sub"/>
+</form>
+</body>
+</html>
+<?php
+?>
