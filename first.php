100c01ac7e3770055a17fbe329addac8109de83c
lcom81_two <lcom81_two>
2017-01-04 12:33:10 +0530

    edited successfully

--- first.php
+++ first.php
@@ -2,9 +2,11 @@
 <head></head>
 <body>
 <form method="post" action="">
-<input type="text" name="name"/>
-<input type="text" name="name"/>
-<input type="text" name="name"/>
+<input type="text" name="fname" placeholder="First Name"/>
+<input type="text" name="lname" placeholder="Last Name"/>
+<input type="submit" name="sub"/>
 </form>
 </body>
 </html>
+<?php
+?>
