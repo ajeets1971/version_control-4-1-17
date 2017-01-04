a4a54a755ce4002d4645bb9daf39eda039e15194
lcom81_two <lcom81_two>
2017-01-04 12:39:02 +0530

    First php file updated

--- first.php
+++ first.php
@@ -5,6 +5,7 @@
 <input type="text" name="fname" placeholder="First Name"/>
 <input type="text" name="lname" placeholder="Last Name"/>
 <input type="submit" name="sub"/>
+<input type="reset" name="sub1"/>
 </form>
 </body>
 </html>
